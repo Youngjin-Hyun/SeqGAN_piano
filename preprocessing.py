c7b3e8f4855abdada7a22050437a72a317ca74e1
Uiwon Hwang <shinyflight@gmail.com>
2017-06-06 22:42:23 +0900

    check the number of parts

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 from music21 import *
 import os
-
+from copy import copy
+import pickle
 
 def load_data(file_path):
     pre_piece = converter.parse(file_path)
@@ -17,21 +18,19 @@ class preprocessing(object):
     def __init__(self):
         self.chords = []
         self.chord_octaves = []
-        self.chord_ref = [[0, 4, 7], [2, 7, 11], [2, 5, 10], [0, 5, 9], [3, 7, 10], [0, 5, 8], [1, 5, 10], [2, 5, 9], [0, 4, 9],
-         [2, 7, 10], [2, 11], [0, 4], [2, 5], [0, 4, 7, 10], [2, 6, 9], [4, 7], [0, 5], [5, 11], [5, 9], [2, 4], [5, 7],
-         [0, 3, 7], [0, 2, 6, 9], [1, 4, 7, 9], [2, 5, 7, 11], [1, 4, 7], [1, 5, 8], [1, 4, 9], [4, 8, 11], [7, 11],
-         [0, 9], [4, 8], [0, 7, 9], [2, 7], [4, 7, 11], [3, 6, 11], [0, 3], [3, 7], [5, 8], [2, 10], [7, 10], [0, 8],
-         [5, 10], [0, 3, 8], [2, 5, 8, 10], [2, 5, 11], [0, 2, 5, 8], [3, 6, 10], [0, 3, 5, 9], [6, 9], [2, 4, 8, 11],
-         [5, 8, 11], [0, 2, 5, 9], [1, 6], [5, 7, 11], [4, 7, 9], [1, 3, 7, 10], [0, 4, 7, 9], [0, 6, 9], [2, 5, 7, 10],
-         [1, 6, 10], [1, 4, 6, 10], [3, 6, 9, 11], [2, 6, 11], [0, 7], [0, 3, 6, 8], [1, 6, 9], [3, 6], [3, 10],
-         [0, 3, 5, 8], [0, 2], [2], [3, 9], [3], [0, 3, 6]]
-        self.octave_ref = [[3, 3, 3], [2, 3, 2], [3, 2, 3], [3, 4, 3], [4, 3, 4], [5, 4], [5, 5], [3, 3, 3, 3], [4, 4], [3, 3, 4, 3],
-         [4, 3, 4, 4], [4, 4, 3, 3], [4, 3], [4, 4, 4], [2, 2, 2], [3, 3, 2, 2], [4, 5], [4, 4, 4, 4], [3, 2, 3, 3],
-         [2, 2], [3, 3], [3, 4, 4], [4, 3, 3], [4, 4, 3], [2, 3, 3], [4, 4, 4, 3], [4, 5, 4], [5, 4, 4, 4],
-         [2, 2, 3, 2], [3, 4, 3, 4]]
-
+        self.chord_ref = ['Rest', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, [0, 4, 7], [2, 7, 11], [2, 5, 10], [0, 5, 9], [3, 7, 10], [0, 5, 8], [1, 5, 10], [2, 5, 9], [0, 4, 9], [2, 7, 10], [2, 11], [0, 4], [2, 5], [0, 4, 7, 10], [2, 6, 9], [4, 7], [0, 5], [5, 11], [5, 9], [2, 4], [5, 7], [0, 3, 7], [0, 2, 6, 9], [1, 4, 7, 9], [2, 5, 7, 11], [1, 4, 7], [1, 5, 8], [1, 4, 9], [4, 8, 11], [7, 11], [0, 9], [4, 8], [0, 7, 9], [2, 7], [4, 7, 11], [3, 6, 11], [0, 3], [3, 7], [5, 8], [2, 10], [7, 10], [0, 8], [5, 10], [0, 3, 8], [2, 5, 8, 10], [2, 5, 11], [0, 2, 5, 8], [3, 6, 10], [0, 3, 5, 9], [6, 9], [2, 4, 8, 11], [5, 8, 11], [0, 2, 5, 9], [1, 6], [5, 7, 11], [4, 7, 9], [1, 3, 7, 10], [0, 4, 7, 9], [0, 6, 9], [2, 5, 7, 10], [1, 6, 10], [1, 4, 6, 10], [3, 6, 9, 11], [2, 6, 11], [0, 7], [0, 3, 6, 8], [1, 6, 9], [3, 6], [3, 10], [0, 3, 5, 8], [0, 2], [2], [3, 9], [3], [0, 3, 6]]
+        self.octave_ref = ['Rest', 1, 2, 3, 4, 5, 6, [3, 3, 3], [2, 3, 2], [3, 2, 3], [3, 4, 3], [4, 3, 4], [5, 4], [5, 5], [3, 3, 3, 3], [4, 4], [3, 3, 4, 3], [4, 3, 4, 4], [4, 4, 3, 3], [4, 3], [4, 4, 4], [2, 2, 2], [3, 3, 2, 2], [4, 5], [4, 4, 4, 4], [3, 2, 3, 3], [2, 2], [3, 3], [3, 4, 4], [4, 3, 3], [4, 4, 3], [2, 3, 3], [4, 4, 4, 3], [4, 5, 4], [5, 4, 4, 4], [2, 2, 3, 2], [3, 4, 3, 4]]
+        """
         self.chords_cnt = [0] * len(self.chord_ref)
         self.chord_octaves_cnt = [0] * len(self.octave_ref)
+
+        self.notes = []
+        self.note_octaves = []
+        self.note_ref = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        self.note_octave_ref = [1, 2, 3, 4, 5, 6]
+        self.notes_cnt = [0] * len(self.note_ref)
+        self.note_octaves_cnt = [0] * len(self.note_octave_ref)
+        """
 
     def streaming(self, part, event, part_tuples):
         # save start time
@@ -47,29 +46,41 @@ class preprocessing(object):
             # save index for sorting pitches of chord
             sort_idx = [i[0] for i in sorted(enumerate(event.pitchClasses), key=lambda x: x[1])]
             octaves = [x for (y, x) in sorted(zip(sort_idx, octaves))]
+            ch_idx = self.chord_ref.index(event.orderedPitchClasses)
+            oc_idx = self.octave_ref.index(octaves)
+            part_tuples.append([offset, event.quarterLength, oc_idx, ch_idx, event.volume.velocity])
 
+            """
             ch_idx = self.chord_ref.index(event.orderedPitchClasses)
             oc_idx = self.octave_ref.index(octaves)
             self.chords_cnt[ch_idx] += 1
             self.chord_octaves_cnt[oc_idx] += 1
-            """
             if event.orderedPitchClasses not in self.chords:
                 self.chords.append(event.orderedPitchClasses)
             if octaves not in self.chord_octaves:
                 self.chord_octaves.append(octaves)
-
             """
 
-            for note in event._notes:
-                part_tuples.append([offset, note.quarterLength, note.pitch.octave, note.pitch.name, note.volume.velocity])
+
         # if current event is note
         if getattr(event, 'isNote', None) and event.isNote:
             # change to key
             # make one step in sequence
-            part_tuples.append([offset, event.quarterLength, event.pitch.octave, event.pitch.name, event.volume.velocity])
+            """
+            if event.pitch.octave not in self.note_octaves:
+                self.note_octaves.append(event.pitch.octave)
+            if event.pitchClass not in self.notes:
+                self.notes.append(event.pitchClass)
+            no_idx = self.note_ref.index(event.pitchClass)
+            oc_idx = self.note_octave_ref.index(event.pitch.octave)
+            self.notes_cnt[no_idx] += 1
+            self.note_octaves_cnt[oc_idx] += 1
+            
+            """
+            part_tuples.append([offset, event.quarterLength, event.pitch.octave, event.pitchClass, event.volume.velocity])
         # if current event is rest
         if getattr(event, 'isRest', None) and event.isRest:
-            part_tuples.append([offset, event.quarterLength, 'Rest', 'Rest', 0])
+            part_tuples.append([offset, event.quarterLength, 0, 0, 0])
         return part_tuples
 
     def parsing(self, data_path):
@@ -97,23 +108,85 @@ class preprocessing(object):
                     part_tuples = self.streaming(part, event, part_tuples)
             if part_tuples != []:
                 all_parts.append(part_tuples)
-        print(piece.analyze('key'))
-        return all_parts
+        # print(piece.analyze('key'))
+        parsed = self.compare_parts(all_parts)
+        sequence = self.sequentialize(parsed)
+        return sequence
 
+    def compare_parts(self, all_parts):
+        if len(all_parts) != 2:
+            print('')
+            #raise ValueError('the number of parts is not two!')
+        melody = all_parts[0]
+        chord = all_parts[1]
+        while 1:
+            for i in range(len(melody)):
+                try:
+                    if melody[i][0] < chord[i][0]:
+                        chord.insert(i, [melody[i][0], melody[i+1][0]-melody[i][0], 0, 0, 0])
+                except:
+                    chord.append([melody[i][0], 0.25, 0, 0, 0])
+            if self.chk_same(melody, chord):
+                return all_parts
 
+            for i in range(len(chord)):
+                try:
+                    if chord[i][0] < melody[i][0]:
+                        melody.insert(i, [chord[i][0], chord[i+1][0]-chord[i][0], 0, 0, 0])
+                except:
+                    # if length of chord is bigger than that of melody
+                    melody.append([chord[i][0], 0.25, 0, 0, 0])
+            if self.chk_same(melody, chord):
+                return all_parts
+
+    def chk_same(self,melody, chord):
+        mel_time = [item[0] for item in melody]
+        cho_time = [item[0] for item in chord]
+        if mel_time == cho_time:
+            return True
+        else:
+            return False
+
+    def sequentialize(self, parsed):
+        if len(parsed[0])!=len(parsed[1]):
+            raise ValueError
+        sequence = []
+        for i in range(len(parsed[0])):
+            token = copy(parsed[0][i][1:])
+            token.extend(parsed[1][i][1:])
+            sequence.append(token)
+        return sequence
 
 
 if __name__ == "__main__":
     a = preprocessing()
-    for file in os.listdir('./Nottingham/all'):
-        all_parts = a.parsing('./Nottingham/all/'+file)
+    data_dir = './Nottingham/all/'
+    dataset = []
+    for file in os.listdir(data_dir):
         print(file)
+        seq = a.parsing(data_dir+file)
+        dataset.append(seq)
+
+    with open('dataset', 'wb') as fp:
+        pickle.dump(dataset, fp)
+
+    # to load dataset
+    # with open('outfile', 'rb') as fp:
+    #     itemlist = pickle.load(fp)
+
+        """
+        print('notes: ', a.notes)
+        print('note_octaves: ', a.note_octaves)
+        print('notes_cnt: ', a.notes_cnt)
+        print('note_octaves_cnt: ', a.note_octaves_cnt)
+        
         print('chords_cnt: ', a.chords_cnt)
         print('octaves_cnt: ', a.chord_octaves_cnt)
-        """
-        print(a.chords)
-        print(a.chord_octaves)
+        print('chords: ',a.chords)
+        print('octaves: ',a.chord_octaves)
+        
         print('len(chords): ',len(a.chords))
         print('len(chord_octaves): ',len(a.chord_octaves))
-        """
         print('\n')
+        """
+
